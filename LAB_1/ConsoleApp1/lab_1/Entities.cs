de0ecd1a1f6cfae0701c2414fa6ed4e173522036
de1ukc <Trapper.MD@mail.ru>
2021-09-10 00:11:13 +0300

    lab_1 finished
    По сути, лаба говно, написанное , после перерыва в 5 месяцев. Когда-нибудь, я , возможно , доработаю это говно, если не повешусь

---
+++ LAB_1/ConsoleApp1/lab_1/Entities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public string name;
+        public int money;
+        public int procent;
+
+        public User()
+        {
+            
+        }
+
+        public User(string name, int money, int procent)
+        {
+            this.name = name;
+            this.money = money;
+            this.procent = procent;
+        }
+        
+    }
+
+    public class Bank
+    {
+        public  MyCustomCollection<MyCustomCollection<User>> arr = new MyCustomCollection<MyCustomCollection<User>>();
+        public  MyCustomCollection<int>  list = new MyCustomCollection<int>();
+
+        public Bank()
+        {
+            int i = 0;
+            while (i < 101)
+            {
+                list.Add(i);
+                i++;
+                arr.Add(new MyCustomCollection<User>());
+            }
+        }
+        
+    }
+}
